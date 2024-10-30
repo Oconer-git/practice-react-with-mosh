8aa4994effac2dd5903fa0b110ff1451eb55eb69
Donell Oconer <donellpie@gmail.com>
2024-10-30 19:58:36 +0800

    managing-components: understanding the strict mode

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,9 @@ import { useState } from 'react';
 import './App.css';
 import Message from './Message';
 function App() {
-  // [false, true]
 
   return (
     <div>
-      <Message/>
-      <Message/>
       <Message/>
     </div>
   )
