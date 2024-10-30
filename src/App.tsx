416cd258ef77d207faf0fcb5c6f02d0586e093b5
Donell Oconer <donellpie@gmail.com>
2024-10-30 19:41:22 +0800

    managing-components: understanding the hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
-import Alert from "./components/Alert";
-import Button from "./components/Button";
-import ListGroup from "./components/ListGroup";
-import Like from "./components/Like";
+
+import { useState } from 'react';
 import './App.css';
-import { FcAbout } from "react-icons/fc";
 
 function App() {
-  const cities = ['New York', 'Miami', 'Los Angeles', 'San Francisco'];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  }
-  const buttonClick = () => {
-    console.log('clicked');
+  const [isVisible, setVisibility] = useState(false);
+  const [isApproved, setApproved] = useState(true);
+
+  const handleClick = () => {
+    setVisibility(true);
+    console.log(isVisible);
   }
 
   return (
     <div>
-      <Like onClick={() => {console.log('clicked')}}/>
+      <button onClick={handleClick}>click</button>
     </div>
   )
  
