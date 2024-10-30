7c8892207ca115fe1d4fdedee21d1f8d59484235
Donell Oconer <donellpie@gmail.com>
2024-10-30 13:43:01 +0800

    styling-components: css modules

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Alert from "./components/Alert";
 import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
 import { useState } from "react";
+import './App.css';
 
 function App() {
   const cities = ['New York', 'Miami', 'Los Angeles', 'San Francisco'];
--- src/components/ListGroup/ListGroup.tsx
+++ src/components/ListGroup/ListGroup.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import './ListGroup.css'
+import styles from "./ListGroup.module.css"
 
 //function
 interface Props{
@@ -14,7 +14,7 @@ function ListGroup({items, heading, onSelectItem} : Props) {
         <>
             <h1>{heading}</h1>
             {items.length === 0 && <p>no items</p>}
-            <ul className="list-group">    
+            <ul className={[styles.ListGroup, styles.container].join(' ')}>    
                 {items.map((item, index) => (
                     <li className={ selectedIndex === index ? "list-group-item active" : "list-group-item"}
                         key={item}
