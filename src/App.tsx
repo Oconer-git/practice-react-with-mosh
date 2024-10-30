c9120ca511b9176f8f9ff481d42d9fa4e10cc93b
Donell Oconer <donellpie@gmail.com>
2024-10-30 20:39:26 +0800

    managing-components: updating arrays

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,21 @@ import { useState } from 'react';
 import './App.css';
 import Message from './Message';
 function App() {
+  const [tags, setTags] = useState(['happy','cheerful']);
+
+  const handleClick = () => {
+    // add
+    setTags([...tags, 'exciting']);
+
+    //remove
+    setTags(tags.filter(tag => tag != 'happy'));
+
+    //update
+    setTags(tags.map(tag => tag === 'happy' ? 'happiness' : tag));
+  }
 
   return (
     <div>
-      <Message/>
     </div>
   )
  
