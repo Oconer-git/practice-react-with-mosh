aa42fc75a1c2cd3b009da3e0968e1f778b572f83
Donell Oconer <donellpie@gmail.com>
2024-10-31 12:17:26 +0800

    managing-components: sharing state between components

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,15 @@
 
 import { useState } from 'react';
 import './App.css';
-import produce from 'immer';
+import Navbar from './components/Navbar';
+import Cart from './components/Cart';
 
 function App() {
-  const [bugs, setBugs] = useState([
-    {id: 1, title: 'Bug 1', fixed: false},
-    {id: 2, title: 'Bug 2', fixed: false},
-  ]);
-
-  const handleClick = () => {
-    //updating array of objects
-    // setBugs(bugs.map(bug => bug.id === 1 ? {...bug, fixed: true} : bug));
-
-    // update bugs with the use of immer
-    setBugs(produce(draft => {
-      const bug = draft.find(bug => bug.id === 1);
-      if(bug) bug.fixed = true;
-    }))
-  }
-
+  const [cartItems, setCartItems] = useState(['Product1', 'Product2']);
   return (
     <div>
-      {bugs.map(bug => 
-        <p key={bug.id}>
-          title:{bug.title} {bug.fixed ? 'fixed' : 'new'}
-        </p>
-      )}
-      <button onClick={handleClick}>button</button>
+      <Navbar cartItemsCount={cartItems.length}/>
+      <Cart cartItems={cartItems} onClear={()=>{setCartItems([])}}/>
     </div>
   )
 }
