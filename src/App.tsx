6a1302ecb6fa6c6fe217d4b20b12c72b40956e50
Donell Oconer <donellpie@gmail.com>
2024-10-31 14:07:06 +0800

    managing-components: exercise-updating state

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,58 @@
 
 import { useState } from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
-import Cart from './components/Cart';
+
 
 function App() {
-  const [cartItems, setCartItems] = useState(['Product1', 'Product2']);
+  const [game, setGame] = useState({
+    id: 1,
+    player: {
+      name: 'Hacob'
+    }
+  })
+
+  const [pizza, setPizza] = useState({
+    name: 'Spicy Pepperoni',
+    toppings: ['Mushroom', 'Pepperoni']
+  })
+
+  const [cart, setCart] = useState({
+    discount: .1,
+    items: [
+      {id: 1, title: 'Product 1', quantity: 1},
+      {id: 2, title: 'Product 2', quantity: 2},
+    ]
+  })
+
+  const handleClickGame = () => {
+    setGame({...game, player:{...game.player, name:'Jacob'}})
+  }
+
+  const handleClickPizza = () => {
+    setPizza({...pizza, toppings:[...pizza.toppings, 'Cheese']})
+  }
+
+  const handleClickCart = () => {
+    setCart({...cart, items: cart.items.map(item => item.id === 1 ? {...item, quantity: item.quantity + 1} : item)})
+  }
+  
   return (
     <div>
-      <Navbar cartItemsCount={cartItems.length}/>
-      <Cart cartItems={cartItems} onClear={()=>{setCartItems([])}}/>
+      <section>
+          {/* exercise 1 */}
+          <p>{game.player.name}</p>
+          <button onClick={handleClickGame}>Click to change name</button>
+      </section>
+      <section>
+        {/* exercise 2 */}
+        {pizza.toppings.map(item => <p key={item}>{item}</p>)}
+        <button onClick={handleClickPizza}>Click to add toppings</button>
+      </section>
+      <section>
+        {/* exerceise 3 */}
+        {cart.items.map(item => <p>title:{item.title} quantity:{item.quantity}</p>)}
+        <button onClick={handleClickCart}>Click to change quantity of cart</button>
+      </section>
     </div>
   )
 }
