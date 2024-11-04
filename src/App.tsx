0cae17d4b57b803f00500f75734bb882bc0d3959
Donell Oconer <donellpie@gmail.com>
2024-11-04 16:36:33 +0800

    connecting-to-the-backend: showing a loading indicator

--- src/App.tsx
+++ src/App.tsx
@@ -11,20 +11,28 @@ interface User{
 function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState('');
-  
+  const [isLoading, setLoading] = useState(false);
+
   useEffect(() => {
     const controller = new AbortController();
+    setLoading(true);
+    
     axios.get<User[]>('https://jsonplaceholder.typicode.com/users',{signal: controller.signal})
-      .then((res) => setUsers(res.data))
+      .then((res) => {
+        setUsers(res.data);
+        setLoading(false);
+      })
       .catch((error) => {
         if(error instanceof CanceledError) return;
         setError(error.message);
+        setLoading(false);
       });
       return () => controller.abort();
   },[])
 
   return (
     <>
+      {isLoading && <div className="spinner-border"></div>}
       {error && <p className="text-danger">{error}</p>}
       <ul>
         {users.map(user => <li key={user.id}>{user.name}</li>)}
