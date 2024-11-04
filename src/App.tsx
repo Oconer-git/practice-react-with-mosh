ccf1143a2d2ab524c1d7127f11a1104bc83fc18b
Donell Oconer <donellpie@gmail.com>
2024-11-04 16:19:15 +0800

    connecting-to-the-backend: cancelling a fetch request

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 
 import { useState, useEffect } from 'react';
-import axios,{AxiosError} from 'axios';
+import axios,{AxiosError, CanceledError} from 'axios';
 
 import './App.css';
 
@@ -11,26 +11,23 @@ interface User{
 function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState('');
-
+  
   useEffect(() => {
-    const fetchUsers = async() => {
-      try{
-        const res = await axios
-        .get<User[]>('https://jsonplaceholder.typicode.com/xusers')
-        setUsers(res.data);
-      }
-      catch(err){
-        setError((err as AxiosError).message);
-      }
-    }
-    fetchUsers();
-  })
+    const controller = new AbortController();
+    axios.get<User[]>('https://jsonplaceholder.typicode.com/users',{signal: controller.signal})
+      .then((res) => setUsers(res.data))
+      .catch((error) => {
+        if(error instanceof CanceledError) return;
+        setError(error.message);
+      });
+      return () => controller.abort();
+  },[])
 
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
       <ul>
-        {users.map(user => <li>{user.name}</li>)}
+        {users.map(user => <li key={user.id}>{user.name}</li>)}
       </ul>
     </>
 
