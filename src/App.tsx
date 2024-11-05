140d56c0fd4f2d274884965eba92261c72cce40f
Donell Oconer <donellpie@gmail.com>
2024-11-05 10:07:41 +0800

    connecting-to-the-backend: connecting a custom data fetching hook

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,7 @@ import userService,{ User } from './services/user-service';
 import './App.css';
 
 function App() {
-  const [users, setUsers] = useState<User[]>([]);
-  const [error, setError] = useState('');
-  const [isLoading, setLoading] = useState(false);
-
-  //get users and mount on the component
-  useEffect(() => {
-    setLoading(true);
-
-    const {request, cancel} = userService.getAll<User[]>();
-    request
-      .then((response) => {
-        setUsers(response.data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        if(error instanceof CanceledError) return;
-        setError(error.message);
-        setLoading(false);
-      });
-      return () => cancel();
-  }, []);
+ 
 
     //delete
     const deleteUser = (user: User) => {
---
+++ src/hooks/useUsers.ts
@@ -0,0 +1,31 @@
+import userService, { User } from "../services/user-service";
+import {useEffect, useState} from 'react';
+import { CanceledError } from '../services/api-client';
+
+const useUsers = () => {
+    const [users, setUsers] = useState<User[]>([]);
+    const [error, setError] = useState('');
+    const [isLoading, setLoading] = useState(false);
+
+    //get users and mount on the component
+    useEffect(() => {
+    setLoading(true);
+
+    const {request, cancel} = userService.getAll<User[]>();
+    request
+        .then((response) => {
+        setUsers(response.data);
+        setLoading(false);
+        })
+        .catch((error) => {
+        if(error instanceof CanceledError) return;
+        setError(error.message);
+        setLoading(false);
+        });
+        return () => cancel();
+    }, []);
+
+    return {users, error, isLoading, setUsers, setError, setLoading};
+}
+
+export default useUsers;
